cda01ff34dc4adbbc54946ed6bdb3120ec6f4aa4
Claudio Clemens <asturio@gmx.net>
2017-10-25 23:18:05 +0200

    Empty Tests only.

---
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfPKCS7Test.java
@@ -0,0 +1,160 @@
+package com.lowagie.text.pdf;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PdfPKCS7Test {
+    @Test
+    public void getDigest() throws Exception {
+    }
+
+    @Test
+    public void getAlgorithm() throws Exception {
+    }
+
+    @Test
+    public void getTimeStampToken() throws Exception {
+    }
+
+    @Test
+    public void getTimeStampDate() throws Exception {
+    }
+
+    @Test
+    public void getOcsp() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void verify() throws Exception {
+    }
+
+    @Test
+    public void verifyTimestampImprint() throws Exception {
+    }
+
+    @Test
+    public void getCertificates() throws Exception {
+    }
+
+    @Test
+    public void getSignCertificateChain() throws Exception {
+    }
+
+    @Test
+    public void getCRLs() throws Exception {
+    }
+
+    @Test
+    public void getSigningCertificate() throws Exception {
+    }
+
+    @Test
+    public void getVersion() throws Exception {
+    }
+
+    @Test
+    public void getSigningInfoVersion() throws Exception {
+    }
+
+    @Test
+    public void getDigestAlgorithm() throws Exception {
+    }
+
+    @Test
+    public void getHashAlgorithm() throws Exception {
+    }
+
+    @Test
+    public void loadCacertsKeyStore() throws Exception {
+    }
+
+    @Test
+    public void loadCacertsKeyStore1() throws Exception {
+    }
+
+    @Test
+    public void verifyCertificate() throws Exception {
+    }
+
+    @Test
+    public void verifyCertificates() throws Exception {
+    }
+
+    @Test
+    public void getOCSPURL() throws Exception {
+    }
+
+    @Test
+    public void isRevocationValid() throws Exception {
+    }
+
+    @Test
+    public void getIssuerFields() throws Exception {
+    }
+
+    @Test
+    public void getSubjectFields() throws Exception {
+    }
+
+    @Test
+    public void getEncodedPKCS1() throws Exception {
+    }
+
+    @Test
+    public void setExternalDigest() throws Exception {
+    }
+
+    @Test
+    public void getEncodedPKCS7() throws Exception {
+    }
+
+    @Test
+    public void getEncodedPKCS71() throws Exception {
+    }
+
+    @Test
+    public void getEncodedPKCS72() throws Exception {
+    }
+
+    @Test
+    public void getAuthenticatedAttributeBytes() throws Exception {
+    }
+
+    @Test
+    public void getReason() throws Exception {
+    }
+
+    @Test
+    public void setReason() throws Exception {
+    }
+
+    @Test
+    public void getLocation() throws Exception {
+    }
+
+    @Test
+    public void setLocation() throws Exception {
+    }
+
+    @Test
+    public void getSignDate() throws Exception {
+    }
+
+    @Test
+    public void setSignDate() throws Exception {
+    }
+
+    @Test
+    public void getSignName() throws Exception {
+    }
+
+    @Test
+    public void setSignName() throws Exception {
+    }
+
+}
